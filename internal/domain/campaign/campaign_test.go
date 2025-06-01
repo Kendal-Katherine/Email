4c13074d8a9300325899ad4bda4e359d24acd103
Kendal-Katherine <kendal.katherine@gmail.com>
2025-06-01 19:32:00 -0300

    test: add validation tests for NewCampaign function parameters

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -16,7 +16,7 @@ var (
 func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	assert := assert.New(t)
 
-	campaign := NewCampaign(name, content, contacts)
+	campaign, _ := NewCampaign(name, content, contacts)
 
 	assert.Equal(name, campaign.Name)
 	assert.Equal(content, campaign.Content)
@@ -27,7 +27,7 @@ func Test_NewCampaign_CreateCampaign(t *testing.T) {
 func TestNewCampaign_IDIsNotNil(t *testing.T) {
 	assert := assert.New(t)
 
-	campaign := NewCampaign(name, content, contacts)
+	campaign, _ := NewCampaign(name, content, contacts)
 
 	assert.NotNil(campaign.ID)
 
@@ -37,8 +37,35 @@ func TestNewCampaign_CreatedOnMustBeNow(t *testing.T) {
 	assert := assert.New(t)
 	now := time.Now().Add(-time.Minute)
 
-	campaign := NewCampaign(name, content, contacts)
+	campaign, _ := NewCampaign(name, content, contacts)
 
 	assert.Greater(campaign.CreatedOn, now)
+
+}
+
+func Test_NewCampaign_MustValidadeName(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign("", content, contacts)
+
+	assert.Equal("name is required", err.Error())
+
+}
+
+func Test_NewCampaign_MustValidadeContent(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, "", contacts)
+
+	assert.Equal("content is required", err.Error())
+
+}
+
+func Test_NewCampaign_MustValidadeContacts(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, content, []string{})
+
+	assert.Equal("contacts is required", err.Error())
 
 }

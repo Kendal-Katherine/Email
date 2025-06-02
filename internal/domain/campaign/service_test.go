c750d41dba24bf7f7ac1d82c4f937c1db6c549b8
Kendal-Katherine <kendal.katherine@gmail.com>
2025-06-01 21:23:51 -0300

    refactor: rename RepositoryMock to repositoryMock and streamline test setup

--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -8,41 +8,38 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type RepositoryMock struct {
+type repositoryMock struct {
 	mock.Mock
 }
 
-func (r *RepositoryMock) Save(campaign *Campaign) error {
+func (r *repositoryMock) Save(campaign *Campaign) error {
 	args := r.Called(campaign)
 	return args.Error(0)
 }
 
-func Test_Create_Campaign(t *testing.T) {
-
-	assert := assert.New(t)
-	service := Service{}
-
-	newCampaign := contract.NewCampaign{
-		Name:    "Test x",
-		Content: "Body",
-		Emails:  []string{"teste@e.com"},
+var (
+	newCampaign = contract.NewCampaign{
+		Name:    "Test Y",
+		Content: "Body Hi!",
+		Emails:  []string{"teste1@test.com"},
 	}
+	service = Service{}
+)
+
+func Test_Create_Campaign(t *testing.T) {
+	assert := assert.New(t)
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("Save", mock.Anything).Return(nil)
+	service.Repository = repositoryMock
 
 	id, err := service.Create(newCampaign)
+
 	assert.NotNil(id)
 	assert.Nil(err)
 }
 
 func Test_Create_SaveCampaign(t *testing.T) {
-
-	repositoryMock := new(RepositoryMock)
-
-	newCampaign := contract.NewCampaign{
-		Name:    "Test x",
-		Content: "Body",
-		Emails:  []string{"teste@e.com"},
-	}
-
+	repositoryMock := new(repositoryMock)
 	repositoryMock.On("Save", mock.MatchedBy(func(campaign *Campaign) bool {
 		if campaign.Name != newCampaign.Name ||
 			campaign.Content != newCampaign.Content ||
@@ -52,7 +49,9 @@ func Test_Create_SaveCampaign(t *testing.T) {
 
 		return true
 	})).Return(nil)
-	service := Service{Repository: repositoryMock}
+	service.Repository = repositoryMock
+
 	service.Create(newCampaign)
+
 	repositoryMock.AssertExpectations(t)
 }

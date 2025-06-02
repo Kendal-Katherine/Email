95c642cf60dc3048c19242cfb90f887aa51dae48
Kendal-Katherine <kendal.katherine@gmail.com>
2025-06-01 21:51:52 -0300

    fix: improve error handling in Create method for campaign service

--- internal/domain/campaign/service.go
+++ internal/domain/campaign/service.go
@@ -2,6 +2,7 @@ package campaign
 
 import (
 	"email/internal/contract"
+	internalerrors "email/internal/internal-errors"
 )
 
 type Service struct {
@@ -10,7 +11,14 @@ type Service struct {
 
 func (s *Service) Create(newCampaign contract.NewCampaign) (string, error) {
 
-	campaign, _ := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
-	s.Repository.Save(campaign)
+	campaign, err := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
+	if err != nil {
+		return "", err
+	}
+	err = s.Repository.Save(campaign)
+	if err != nil {
+		return "", internalerrors.ErrInternal
+	}
+
 	return campaign.ID, nil
 }
